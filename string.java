6e42d255cd96ccfa1b15b4ed58b39f01675708be
SURIYALAKSHMI M <107531525+SURIYALAKSHMI29@users.noreply.github.com>
2023-10-09 11:49:20 +0530

    string.java

--- string.java
+++ string.java
@@ -4,9 +4,9 @@ public class Strings {
     {
         String s=new String("Land");
         s.concat("mister").replace('d', 'n').toUpperCase().replace('M','N').toString();
-        System.out.println(s);
+        System.out.println(s); //output : Land
         String str ="oops";
-        String str2 = new String("oops");
+        String str2 = new String("oops"); //output = false
         System.out.println(str==str2);
     }  
 }
